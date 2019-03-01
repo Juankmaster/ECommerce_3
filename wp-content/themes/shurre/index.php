3240ab0c5cb1742a24443e608d651b5519997aa4
juankMaster <sekureidtecnico@gmail.com>
2019-02-28 23:16:07 -0500

    version 1.2

--- wp-content/themes/shurre/index.php
+++ wp-content/themes/shurre/index.php
@@ -34,8 +34,8 @@ get_header();
 					<div class="card-content">
 						<span class="card-title activator grey-text text-darken-4"><?php the_title(); ?></span>
 						<p><?php the_excerpt(); ?></p><br>
-
-		<p><a class="waves-effect waves-light btn"><i class="material-icons left">local_dining</i> <?php the_category('name'); ?></a></p>
+						
+						<p><a class="waves-effect waves-light btn"><i class="material-icons left">local_dining</i> Platos</a></p>
 						<p><a class="waves-effect waves-light btn"><i class="material-icons left">label_outline</i>Ocasiones</a></p>
 						<p><a class="waves-effect waves-light btn"><i class="material-icons left">place</i>Regiones</a></p>
 
